581932e460a3961aafa7ff232e51febdab636a02
austin <167563184+austinkden@users.noreply.github.com>
2026-02-07 13:11:27 -0700

    Fix some things up

--- admin/admin.js
+++ admin/admin.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -15,10 +9,6 @@ const firebaseConfig = {
   appId: "1:156783766681:web:ee2d859d4372859a909c08",
   measurementId: "G-RM0Q3H3XYL"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // Admin Config
 const ADMIN_PASSWORD = "0000"; // The shared password
