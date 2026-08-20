f5836b15bf1804bbf74da89476ad6bc9d3b72dc1
austin <167563184+austinkden@users.noreply.github.com>
2026-08-20 07:09:16 -0600

    3.4.2 hotfix 4

--- js/bar.js
+++ js/bar.js
@@ -405,7 +405,7 @@ class ScheduleTracker {
     updateCredits() {
         const creditsEl = document.querySelector('.credits');
         if (creditsEl) {
-            creditsEl.textContent = `Created by Austin Strong • Version 3.2.0 • ${this.remote.id}`;
+            creditsEl.textContent = `Created by Austin Strong • Version 3.4.2 Hotfix 4 • ${this.remote.id}`;
         }
     }
 
